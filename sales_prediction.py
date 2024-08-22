cd6622a576a548179c6e9962d41e3c3ab4fcb53f
Gøpí Krìẞhñæ Çhègôñï <118162524+gopikrishna-8@users.noreply.github.com>
2024-08-22 12:35:32 +0530

    superstore analysis

--- sales_prediction.py
+++ sales_prediction.py
@@ -1,16 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 
 df = pd.read_csv('Advertising.csv')
 print(df.head())
-
-
-# In[2]:
 
 
 import matplotlib.pyplot as plt
@@ -23,11 +14,6 @@ plt.ylabel('Sales (in thousands)')
 plt.show()
 
 # Similar plots for Radio and Newspaper
-# ...
-
-
-# In[3]:
-
 
 from sklearn.preprocessing import StandardScaler
 
@@ -36,25 +22,16 @@ X = df[['TV', 'Radio', 'Newspaper']]
 X = scaler.fit_transform(X)
 
 
-# In[4]:
-
-
 from sklearn.model_selection import train_test_split
 
 Y = df['Sales']
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, random_state=50, test_size=0.25)
 
 
-# In[5]:
-
-
 from sklearn.linear_model import LinearRegression
 
 clf = LinearRegression()
 clf.fit(X_train, Y_train)
-
-
-# In[6]:
 
 
 from sklearn.metrics import mean_squared_error
@@ -64,18 +41,12 @@ mse = mean_squared_error(Y_test, Y_pred)
 print("Mean Squared Error:", mse)
 
 
-# In[7]:
-
-
 import statsmodels.api as sm
 
 X2 = sm.add_constant(X)
 est = sm.OLS(Y, X2)
 est2 = est.fit()
 print(est2.summary())
-
-
-# In[ ]:
 
 
 
